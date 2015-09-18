fcfaa143802fad58f09381ce925c1b085f8666b1
Lindsay Culver <leculver@gmail.com>
2015-09-18 15:37:31 -0700

    refactor specs.

--- spec/specs.js
+++ spec/specs.js
@@ -2,12 +2,12 @@ describe('Pizza', function() {
   it("creates a new pizza based on user input", function() {
     var testPizza = new Pizza("small", 2, "cheese");
     expect(testPizza.pizzaSize).to.equal("small");
-    expect(testPizza.pizzaQuantity).to.equal(2);
-    expect(testPizza.pizzaType).to.equal("cheese");
+    expect(testPizza.numberOfPizzas).to.equal(2);
+    expect(testPizza.pizzaTopping).to.equal("cheese");
   });
 
   it("calculates the new pizza price based on user input", function() {
     var testPizza = new Pizza("small", 2, "cheese");
-    expect(testPizza.totalCost()).to.equal(32);
+    expect(testPizza.totalCost()).to.equal(34);
   });
 });
