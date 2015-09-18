d9bc1ab54564fe1fc2801399d89f452909e57afd
Lindsay Culver <leculver@gmail.com>
2015-09-18 14:54:32 -0700

    add styling.

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,8 @@ Pizza.prototype.totalCost = function() {
   var toppingCost = (toppingQuantity * 2);
   var pizzaSizeCost = 0;
 
+// radio button option for classic pizza choices for version 2.0
+
   if (this.pizzaSize === "large") {
     pizzaSizeCost = 20;
   } else if (this.pizzaSize === "medium") {
@@ -35,41 +37,3 @@ $(document).ready(function() {
     $("#totalCost").text("$" + newPizza.totalCost());
   });
 });
-
-// function Pizza(pizzaSize, pizzaQuantity, pizzaType) {
-//   this.pizzaSize = pizzaSize;
-//   this.pizzaQuantity = pizzaQuantity;
-//   this.pizzaType = pizzaType;
-// }
-//
-// Pizza.prototype.totalCost = function() {
-//   var toppingType = this.pizzaType.split(" ");
-//   var toppingQuantity = toppingType.length;
-//   var toppingCost = (toppingQuantity * 2);
-//   var pizzaSizeCost = 0;
-//
-//   if(this.pizzaSize === "large") {
-//     pizzaSizeCost = 20;
-//   } else if (this.pizzaSize === "medium") {
-//     pizzaSizeCost = 18;
-//   } else if (this.pizzaSize === "small") {
-//     pizzaSizeCost = 15;
-//   } else {
-//     pizzaSizeCost = 24;
-//   }
-//
-//   var totalCost = toppingCost + (pizzaSizeCost * (parseInt(this.pizzaQuantity)));
-//   return totalCost;
-// };
-//
-// $(documetnt).ready(function() {
-//   $("form#pizzaOrder").submit(function(event) {
-//     event.preventDefault();
-//
-//     var pizzaToppings = $("input#topping").val().toString();
-//     var pizzaSize = $("select#size").val();
-//     var pizzaQuantity = $("input#numberOfPizzas").val();
-//     var newPizza = new Pizza(pizzaSize, pizzaQuantity, pizzaToppings);
-//     $("#totalCost").text("Your total is: $" + newPizza.totalCost());
-//   });
-// });
